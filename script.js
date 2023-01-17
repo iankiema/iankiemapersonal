12007ce906b188a764ea5625d8bbc02d36fa9bc3
Ian Kiema <112932127+iankiema@users.noreply.github.com>
2023-01-17 17:49:16 +0300

    Portfolio files

---
+++ script.js
@@ -0,0 +1,43 @@
+/*----Menu show y hidden----*/
+const navMenu = document.getElementById("nav-menu"),
+      toggleMenu = document.getElementById("nav-toggle"),
+      closeMenu = document.getElementById('nav-close')
+
+// ------show-----
+toggleMenu.addEventListener('click',()=>{
+  navMenu.classList.toggle('show')
+})
+
+//------Hide-----
+closeMenu.addEventListener('click',()=>{
+  navMenu.classList.remove('show')
+})
+
+//------Remove Menu-----
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+  navMenu.classList.remove('show') 
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+//-----Scroll sections active link-----
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll',scrollActive)
+
+function scrollActive(){
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current =>{
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50
+    sectionid = current.getAttribute('id')
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+      document.querySelector('.nav_menu a[href*=' + sectionid +']').classList.add('active')
+    }else{
+      document.querySelector('.nav_menu a[href*=' + sectionid +']').classList.remove('active')
+    }
+  })
+}
